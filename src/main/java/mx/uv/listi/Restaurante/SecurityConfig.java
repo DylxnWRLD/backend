76ed8e78c14f2e795b04ffa361ce33d548257c29
DylxnWRLD <dylaniscute.23@gmail.com>
2025-12-08 19:27:26 -0600

    Despliegue 6

--- src/main/java/mx/uv/listi/Restaurante/SecurityConfig.java
+++ src/main/java/mx/uv/listi/Restaurante/SecurityConfig.java
@@ -2,6 +2,7 @@ package mx.uv.listi.Restaurante;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,6 +20,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().permitAll()
             );
 
